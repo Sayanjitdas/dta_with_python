91c0f4ab6bd08649659beac0b5cafd88e9e05d75
sayanjitdas <sayanjitgit@gmail.com>
2023-09-04 09:03:14 +0530

    commit: pop method done for DLL..

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -24,7 +24,7 @@ class DoublyLinkedList:
 
 
     def append(self,value):
-        """ DLL is same as LL but with another node link to 
+        """ DLL append is same as LL but with another node link to 
         prevoius node
         Time complexity --> BigO(1)
         Args:
@@ -40,6 +40,27 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
 
+    def pop(self):
+        """DLL pop is a bit different than LL, we don't have to iterate
+        through the List to find the node to pop cause DLL has prev link
+        so we can use it to pop and move the tail node
+        Time complexity --> BigO(1)
+        """
+        if self.head is None:
+            raise ValueError("DLL is empty!!")
+        if self.length == 1:
+            node = self.head
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return node
+        else:
+            node = self.tail
+            self.tail = node.prev
+            self.tail.next = None
+            self.length -= 1
+            return node
+
 
     def print_list(self):
         temp = self.head
@@ -51,4 +72,6 @@ class DoublyLinkedList:
 if __name__ == "__main__":
     dll = DoublyLinkedList(400)
     dll.append(101)
+    print(dll.pop().value)
+    # print(dll.pop().value)
     dll.print_list()
